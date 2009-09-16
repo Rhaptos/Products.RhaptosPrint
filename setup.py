c3a1cf4a9f8ebcc5af5fb987fda822b7eedf1638
Josh LaPlace <josh@cnx.rice.edu>
2009-09-16 04:37:34 +0000

    start on product eggification

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.1'
+
+setup(name='Products.RhaptosPrint',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      tests_require = [
+           'zope.testing>=3.5',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
