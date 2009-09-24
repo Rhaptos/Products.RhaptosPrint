5e192f3dd508aa813bbc84387d1d1f1e4e4546fd
Josh LaPlace <josh@cnx.rice.edu>
2009-09-24 23:45:31 +0000

    CONNEXIONSSRV-34: Refactor to use GenericSetup

--- Products/RhaptosPrint/Extensions/Install.py
+++ Products/RhaptosPrint/Extensions/Install.py
@@ -1,20 +1,12 @@
+
 from Products.CMFCore.utils import getToolByName
-from StringIO import StringIO
-import string
 
-def install(self):
-    
-    # Add the tool
-    out = StringIO()
-    urltool = getToolByName(self, 'portal_url')
-    portal = urltool.getPortalObject();
-    try:
-        portal.manage_delObjects('Print Tool')
-        out.write("Removed old print tool\n")
-    except:
-        pass  # we don't care if it fails
-    portal.manage_addProduct['RhaptosPrint'].manage_addTool('Print Tool', None)
-    
-    out.write("Adding Print Tool\n")
+def install(portal):
+    portal_setup = getToolByName(portal, 'portal_setup')
+    import_context = portal_setup.getImportContextID()
+    portal_setup.setImportContext(
+            'profile-Products.RhaptosPrint:default')
+    portal_setup.runAllImportSteps()
+    portal_setup.setImportContext(import_context)
 
-    return out.getvalue()
+
