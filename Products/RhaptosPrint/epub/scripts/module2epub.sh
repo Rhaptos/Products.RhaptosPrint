622bb863adbe956b909d8567a97b29c3772d3106
Philip Schatz <phil@cnx.org>
2010-06-04 20:19:17 +0000

    Removes duplicate glossary entries.

--- Products/RhaptosPrint/epub/scripts/module2epub.sh
+++ Products/RhaptosPrint/epub/scripts/module2epub.sh
@@ -2,8 +2,9 @@
 
 # 1st arg is the path to the collection
 # 2nd arg (optional) is the module name
-
+# 3rd arg is the epub zip file
 COL_PATH=$1
+EPUB_FILE=$3
 
 ROOT=`dirname "$0"`
 ROOT=`cd "$ROOT/.."; pwd` # .. since we live in scripts/
@@ -30,23 +31,24 @@ fi
 # XSL files
 DOCBOOK_CLEANUP_XSL=$ROOT/xsl/dbk-clean-whole.xsl
 DOCBOOK_NORMALIZE_PATHS_XSL=$ROOT/xsl/dbk2epub-normalize-paths.xsl
+DOCBOOK_NORMALIZE_GLOSSARY_XSL=$ROOT/xsl/dbk-clean-whole-remove-duplicate-glossentry.xsl
 
 if [ ".$2" != "." ]; then 
   MODULE=$2;
   bash $ROOT/scripts/module2dbk.sh $COL_PATH $MODULE
-  DBK_FILE=$COL_PATH/$MODULE/index.dbk
-  EPUB_FILE=$COL_PATH/$MODULE.epub.zip
+  DBK_FILE=$COL_PATH/index.dbk
 else
   MODULES=`ls $COL_PATH`
   bash $ROOT/scripts/collection2dbk.sh $COL_PATH
   
   # Clean up image paths
   DOCBOOK=$COL_PATH/collection.dbk
-  DOCBOOK2=$COL_PATH/collection.normalized.dbk
+  DOCBOOK2=$COL_PATH/_collection.normalized.dbk
+  DOCBOOK3=$COL_PATH/_collection3.dbk
   DBK_FILE=$COL_PATH/collection.cleaned.dbk
   $XSLTPROC --xinclude -o $DOCBOOK2 $DOCBOOK_NORMALIZE_PATHS_XSL $DOCBOOK
-  $XSLTPROC -o $DBK_FILE $DOCBOOK_CLEANUP_XSL $DOCBOOK2
-  EPUB_FILE=$COL_PATH.epub.zip
+  $XSLTPROC -o $DOCBOOK3 $DOCBOOK_CLEANUP_XSL $DOCBOOK2
+  $XSLTPROC -o $DBK_FILE $DOCBOOK_NORMALIZE_GLOSSARY_XSL $DOCBOOK3
 fi
 
 $ROOT/docbook-xsl/epub/bin/dbtoepub --stylesheet $ROOT/xsl/dbk2epub.xsl -c $ROOT/content.css -d $DBK_FILE -o $EPUB_FILE
---
+++ Products/RhaptosPrint/epub/xsl/dbk-clean-whole-remove-duplicate-glossentry.xsl
@@ -0,0 +1,25 @@
+<?xml version="1.0" ?>
+<xsl:stylesheet 
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
+  xmlns:mml="http://www.w3.org/1998/Math/MathML"
+  xmlns:c="http://cnx.rice.edu/cnxml"
+  xmlns:db="http://docbook.org/ns/docbook"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  xmlns:md="http://cnx.rice.edu/mdml/0.4" xmlns:bib="http://bibtexml.sf.net/"
+  xmlns:xi="http://www.w3.org/2001/XInclude"
+  version="1.0">
+
+<!-- This file is run after the book-level glossary is created.
+	It removes duplicate db:glossentry elements
+ -->
+
+<xsl:import href="debug.xsl"/>
+<xsl:import href="ident.xsl"/>
+
+<xsl:output indent="yes" method="xml"/>
+
+<xsl:template match="db:glossentry[normalize-space(db:glossterm/text())!='' and db:glossterm/text()=preceding-sibling::db:glossentry/db:glossterm/text()]">
+	<xsl:call-template name="cnx.log"><xsl:with-param name="msg">INFO: Removing duplicate glossentry for term "<xsl:value-of select="db:glossterm/text()"/>"</xsl:with-param></xsl:call-template>
+</xsl:template>
+
+</xsl:stylesheet>
